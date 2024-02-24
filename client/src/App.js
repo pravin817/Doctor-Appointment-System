a5bf4354632d9af862a49e32a5695c33122dbb32
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-02-24 15:24:54 +0530

    The react-router-dom setup done successfully

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 const App = () => {
   return (
-    <div className="bg-dark">
-      <h1 className="text-success">Hello</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
